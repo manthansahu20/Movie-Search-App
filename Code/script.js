578faee8c868e3d1f58162e5834d94d3455b1eb0
Manthan Sahu <manthansahusahu@gmail.com>
2025-12-07 01:01:55 +0530

    A

---
+++ Code/script.js
@@ -0,0 +1,36 @@
+const movieInput = document.getElementById("movieInput");
+const searchBtn = document.getElementById("searchBtn");
+const resultDiv = document.getElementById("result");
+
+searchBtn.addEventListener("click", () => {
+    let movieName = movieInput.value.trim();
+
+    if (movieName === "") {
+        resultDiv.innerHTML = "<p>Please enter a movie name</p>";
+        return;
+    }
+
+    fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=d0d2c897`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            if (data.Response === "True") {
+                let movies = data.Search;
+
+                resultDiv.innerHTML = movies.map(movie => `
+                    <div class="movie-card">
+                        <img src="${movie.Poster}" alt="${movie.Title}">
+                        <h3>${movie.Title}</h3>
+                        <p>${movie.Year}</p>
+                    </div>
+                `).join("");
+            } else {
+                resultDiv.innerHTML = "<p>No movie found 😕</p>";
+            }
+        })
+        .catch(error => {
+            resultDiv.innerHTML = "<p>Error fetching data ❌</p>";
+            console.error(error);
+        });
+});
